63d82f5785ab1b0f2392b2658338df3aeb399dab
supopur <matylda.smyk.08@gmail.com>
2023-03-05 09:51:18 +0100

    cogs now work. Also i added logging and some try catch thingies. I will convert the web interface into a cog the next commit or atleast i hope

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-import discord, flask, os, quart, sys, toml, time, datetime
+import discord, flask, os, quart, sys, toml, time, datetime, logging
 from quart import render_template
 from quart.helpers import make_response
 from discord.ext import commands
 
 
+
+
+
+
+
+
 with open("config.toml", "r") as f:
     config = toml.load(f)
 
-token = os.environ.get('MEETOO_TOKEN')
+if config["webserver"]["debug"] == "true":
+    logging.basicConfig(filename='latest.log', encoding='utf-8', level=logging.DEBUG)
+else:
+    logging.basicConfig(filename='latest.log', encoding='utf-8', level=logging.INFO)
+
+logging.getLogger().addHandler(logging.StreamHandler())
+
+
+def log(level, log):
+    if level == "inf":
+        logging.info(log)
+    elif level == "wrn":
+        logging.warning(log)
+    elif level == "dbg":
+        logging.debug(log)
+    elif level == "err":
+        logging.error(log)
+
+log("inf", "Logging utility set up.")
+
+try:
+    token = os.environ.get('MEETOO_TOKEN')
+except:
+    log("err", 'Error no token is present type: export MEETOO_TOKEN="[Your token here]" into your terminall NOW!')
+
+if token == "":
+    log("err", 'Error no token is present type: export MEETOO_TOKEN="[Your token here]" into your terminall NOW!')
+
+log("dbg", f"Token is: {token}")
 
 start_time = time.time()
 
@@ -17,24 +51,39 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+logging.debug("Loaded the client.")
+
+#Load all the extensions
+for x in config["bot"]["cogs"]:
+    log("inf", f"Loading {x}...")
+    try:
+        bot.load_extension(x)
+    except Exception as e:
+        log("wrn", f"{x} Failed to load skipping... Error: {e}")
+log("inf", "Loading all cogs completed.")
+
+
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    log("inf", f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=["823188238260633600"])
-async def hello(ctx):
-    await ctx.respond("Hello!")
+
 
 @bot.slash_command(guild_ids=["823188238260633600"])
 async def stop(ctx):
     if ctx.author.guild_permissions.administrator:
         print("Terminating the bot...")
-        await ctx.respond("https://media4.giphy.com/media/CC5MVO9Jx4RqMQRfvT/giphy.gif")
+        try:
+            await ctx.respond("https://media4.giphy.com/media/CC5MVO9Jx4RqMQRfvT/giphy.gif")
+        except:
+            pass
         sys.exit("Terminated.")
     else:
         await ctx.respond("https://media.tenor.com/Iv6oKRuAhVEAAAAC/hal9000-im-sorry-dave.gif")
+log("inf", "Loading Quart...")
 #webserver
 app = quart.Quart(__name__)
+log("inf", "Quart loaded.")
 #The index.
 @app.route('/', methods=['GET'])
 async def index():
